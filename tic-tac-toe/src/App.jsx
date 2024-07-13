7228f7e9977a0ca407aa76be26ded490bea031ba
ManuelQuijada <manuelquijada2000@gmail.com>
2024-07-13 23:26:54 +0200

    useTtset import

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,4 +1,4 @@
-import react, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import confetti from "canvas-confetti"
 import { Square } from './components/Square';
